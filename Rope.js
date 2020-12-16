cf736098ea6f003041e8a9e58bc918cd149a3809
saragupta2008 <74290865+saragupta2008@users.noreply.github.com>
2020-12-16 19:19:04 +0530

    Add files via upload

--- Rope.js
+++ Rope.js
@@ -1,10 +1,10 @@
 class Rope{
-    constructor(bodyA,bodyB,offsetX,offsetY){
+    constructor(body1,body2,offsetX,offsetY){
         this.offsetX=offsetX
         this.offsetY=offsetY
         var options={
-            bodyA:bodyA,
-            bodyB:bodyB,
+            bodyA:body1,
+            bodyB:body2,
             pointB:{x:this.offsetX,y:this.offsetY}
         }
         this.rope=Constraint.create(options)
--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
+const Constraint = Matter.Constraint;
 var bobObject1,bobObject2,bobObject3,bobObject4,bobObject5,roof;
-var rope1;
+var rope1,rope2,rope3,rope4,rope5,bobDiameter;
 function preload()
 {
 	
@@ -17,7 +17,7 @@ function setup() {
 
 	engine = Engine.create();
 	world = engine.world;
-
+   bobDiameter=40
 	//Create the Bodies Here.
 	bobObject1= new Bob(200,600,100);
 	bobObject2= new Bob(300,600,100);
@@ -26,7 +26,10 @@ function setup() {
 	bobObject5= new Bob(600,600,100);
 	roof = new Roof(width/2,100,width,20);
 	rope1=new Rope(bobObject1.body,roof.body,-bobDiameter*2,0)
-	
+	rope2=new Rope(bobObject2.body,roof.body,-bobDiameter*2,0)
+    rope3=new Rope(bobObject3.body,roof.body,-bobDiameter*2,0)
+    rope4=new Rope(bobObject4.body,roof.body,-bobDiameter*2,0)
+    rope5=new Rope(bobObject5.body,roof.body,-bobDiameter*2,0)
 
 	Engine.run(engine);
   
@@ -35,15 +38,21 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(255);
   
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
   bobObject1.display();
   bobObject2.display();
   bobObject3.display();
   bobObject4.display();
   bobObject5.display();
 
-  rope1.display();
+  
   roof.display();
   drawSprites();
  
